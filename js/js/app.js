ec9a89ca316096e93b88609c9f62f4fc6e5a70b8
gadifinder <tanmay7773y@gmail.com>
2026-09-20 01:01:09 +0530

    Create app.js

---
+++ js/js/app.js
@@ -0,0 +1,50 @@
+{
+    id: "MARUTI-SWIFT-VXI-001",
+
+    brand: "Maruti Suzuki",
+
+    model: "Swift",
+
+    variant: "VXi",
+
+    price: 700000,
+
+    fuel: "Petrol",
+
+    transmission: "Manual",
+
+    bodyType: "Hatchback",
+
+    seats: 5,
+
+    engine: "1197 cc",
+
+    power: "82 PS",
+
+    torque: "112 Nm",
+
+    mileage: "24.8 km/l",
+
+    bootSpace: "265 L",
+
+    groundClearance: "163 mm",
+
+    imageCard: "",
+
+    imageProfile: "",
+
+    imageFront: "",
+
+    imageRear: "",
+
+    imageSide: "",
+
+    imageInterior: "",
+
+    imageDashboard: "",
+
+    imageExtra: "",
+
+    description:
+        "Add complete vehicle description here."
+},
